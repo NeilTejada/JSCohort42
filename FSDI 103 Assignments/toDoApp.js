7d23c3308093859f5e9e0aa9ecc6e80718e2b40b
neiltejada <Neiltejada86@gmail.com>
2023-10-10 15:45:26 -0700

    todo app

---
+++ FSDI 103 Assignments/toDoApp.js
@@ -0,0 +1,24 @@
+let toDoArray = [];
+
+function addToDo() {
+  const addInput = document.getElementById("add-input");
+  const addList = document.getElementById("add-list");
+  const addCount = document.getElementById("add-count");
+
+  const addNew = addInput.value.trim();
+  if (addNew === " ") {
+    alert("Please Enter a New Task ");
+    return;
+  }
+  toDoArray.push(addNew);
+
+  addInput.value = " ";
+
+  const addItem = document.createElement("li");
+  addItem.textContent = addNew;
+  addList.appendChild(addItem);
+
+  addCount.textContent = toDoArray.length;
+
+  console.log("Tasks: ", toDoArray);
+}
